4d1b6c5b821fec1f30184a758e8d56e5f374a599
Raghav S <20samavedam_raghav@dublinstudents.net>
2017-04-07 11:56:22 -0400

    First commit

---
+++ src/rgs_helloWorld/rgs_helloWorld.java
@@ -0,0 +1,17 @@
+package rgs_helloWorld;
+
+public class rgs_helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =9, b = 4;
+		char var = 'c';
+		a = var + 2;
+		double z = 94.32;
+		z = var + 5;
+		var = 'A' + 4;		
+		System.out.println( var );
+
+	}
+
+}
